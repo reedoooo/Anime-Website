ac9b0b87306c301299b70c333d1de5613ce6d38e
reedoooo <readvogt@gmail.com>
2023-01-05 15:47:58 -0800

    gixed a lot of stuff on website and added a loop for repeating images at login

--- index.js
+++ index.js
@@ -2,25 +2,30 @@
     
     let userPrompt = confirm("Is it alright if I ask you something?"); //creates variable userPrompt and sets value to a confirmation form yes or no
     let favoriteAnime = prompt("What's your favorite anime?"); //create variable and sets value to a confirmation form, answer contingent
-
+    let userAns = favoriteAnime;
     if (userPrompt) { //conditional if the user presses yes
         console.log("Great!");  //display great on console
     } else { //conditional pt 2
         console.log("You suck"); //otherwise display you suck on console
     }
 
+
+
     callFunction = function(){
-        if (favoriteAnime === "Naruto") { //conditional if the answer to the prompt is Naruto
-            console.log("Welcome!"); //have the console log display welcome
-        } else if (favoriteAnime === ""){
-            prompt("Anything animated counts!");
-            document.getElementById("dope").innerHTML = favoriteAnime;
-        } else if (favoriteAnime != null){
-            console.log(`I love ${favoriteAnime}! Welcome to the website!`); // otherwise display I love (answer from form submission). welcome to website
-            document.getElementById("dope").innerHTML = favoriteAnime;
+
+        while (!userAns) {
+            userAns = prompt("What's your favorite anime?!");
+            document.getElementById("dope2").innerHTML = userAns;
+            if (!userAns) {
+              alert("You must provide an answer!");
+            } else {
+              document.getElementById("dope").innerHTML = userAns;
+            }
         }
     }
-    
+
+
+
     extractInfo = function() {
         // Get the value of the input element
         var inputValue = document.getElementById("input-element").value;
@@ -29,3 +34,18 @@
         document.getElementById("output-div").innerHTML = inputValue;
       }
 
+      /*  
+          let anythingAnime = prompt("Anything animated counts!");
+             if (favoriteAnime === "Naruto") { //conditional if the answer to the prompt is Naruto
+            console.log("Welcome!"); //have the console log display welcome
+        } else if (favoriteAnime === "" || favoriteAnime != null){
+            document.getElementById("dope").innerHTML = favoriteAnime;
+        } else if (anythingAnime != null || anythingAnime === ""){
+            console.log(`I also love ${anythingAnime}! Welcome to the website!`); // otherwise display I love (answer from form submission). welcome to website
+            document.getElementById("dope2").innerHTML = anythingAnime;
+        } else if (userAns === String(anythingAnime)) {
+            document.getElementById("dope2").innerHTML = anythingAnime;
+        } else {
+            favoriteAnime;
+        }
+        */
--- login.js
+++ login.js
@@ -1,10 +1,55 @@
 function myFunction() {
-    var x = document.getElementById("frm1");
+  // retreive the form element, create empty text string variable, concatenates value to string 'text'
+    var x = document.getElementById("form");
     var text = "";
     var i;
     for (i = 0; i < x.length ;i++) {
       text += x.elements[i].value + "<br>";
     }
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("ptag").innerHTML = text;
   }
 
+  function showAnimeImage(divElement) {
+    // create image element and set its src attribute to the URL of an anime image
+    var imgElement = document.createElement("img");
+    imgElement.src = "https://images6.alphacoders.com/415/thumb-1920-415519.jpg";
+  
+    // append the image to the div element. The divElement is the parent and the appendChild make the imgElement the child
+    divElement.appendChild(imgElement);
+  }
+  
+  function promptAndShowAnimeImages() {
+    var numImages = prompt("How many anime characters do you want to see?");
+  
+    // get a reference to the div element
+    var divElement = document.getElementById("anime-div");
+  
+    // use a loop to show the image that many times
+    for (var i = 0; i < numImages; i++) {
+      showAnimeImage(divElement);
+    }
+  }
+  
+  // call the function to show the images
+  promptAndShowAnimeImages();
+/*
+  function showAnimeImage() {
+// create an image element and set its src attribute to the URL of an anime image
+var imgElement = document.createElement("img");
+imgElement.src = "https://images6.alphacoders.com/415/thumb-1920-415519.jpg";
+
+// append the image to the body of the page
+document.body.appendChild(imgElement);
+}
+
+function promptAndShowAnimeImages() {
+var numImages = prompt("How many anime characters do you want to see?");
+
+// use a loop to show the image that many times
+for (var i = 0; i < numImages; i++) {
+showAnimeImage();
+}
+}
+
+// call the function to show the images
+promptAndShowAnimeImages(); */
